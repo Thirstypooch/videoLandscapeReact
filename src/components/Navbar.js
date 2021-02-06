d853f91c002233a6c7e07808f4acdb6f9e13d359
Thirstypooch <thirstypooch@protonmail.com>
2021-02-06 02:39:25 -0500

    starting navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false)
   return (
     <>
-    <nav className='navbar'>
-      <div className='navbar-container'>
-
-      </div>
-    </nav>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            YOU Organic Skincare <i class="fab fa-envira"></i>
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                <i class="fas fa-home"></i>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </>
-  )
+  );
 }
+
+export default Navbar;
