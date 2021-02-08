9be3699e3cea43ea9ae7ad4e2add2d1676884b2e
Thirstypooch <thirstypooch@protonmail.com>
2021-02-08 17:17:48 -0500

    doing the hero video

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from './Button'
 import './Navbar.css'
@@ -18,14 +18,17 @@ function Navbar() {
     }
   }
 
+  useEffect(() => {
+    showButton()
+  }, [])
+
   window.addEventListener('resize', showButton)
   return (
     <>
       <nav className='navbar'>
         <div className='navbar-container'>
-          <Link to='/' className='navbar-logo'>
-            YOU Organic Skincare
-            <i class='fab fa-envira' style={{ marginLeft: '.5rem' }}></i>
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+            You Organic Skincare
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
@@ -33,27 +36,27 @@ function Navbar() {
           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
             <li className='nav-item'>
               <Link to='/misión' className='nav-links' onClick={closeMobileMenu}>
-              <i class="fas fa-american-sign-language-interpreting fa-2x"></i>
+              <i className="fas fa-american-sign-language-interpreting fa-2x"></i>
               </Link>
             </li>
             <li className='nav-item'>
               <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
-              <i class="fas fa-barcode fa-2x"></i>
+              <i className="fas fa-barcode fa-2x"></i>
               </Link>
             </li>
             <li className='nav-item'>
               <Link to='/packs' className='nav-links' onClick={closeMobileMenu}>
-              <i class="fas fa-dolly-flatbed fa-2x"></i>
+              <i className="fas fa-dolly-flatbed fa-2x"></i>
               </Link>
             </li>
             <li className='nav-item'>
               <Link to='/limpieza' className='nav-links' onClick={closeMobileMenu}>
-              <i class="fas fa-leaf fa-2x"></i>
+              <i className="fas fa-leaf fa-2x"></i>
               </Link>
             </li>
             <li className='nav-item'>
               <Link to='/blog' className='nav-links' onClick={closeMobileMenu}>
-                <i class='far fa-comment-dots fa-2x'></i>
+                <i className='far fa-comment-dots fa-2x'></i>
               </Link>
             </li>
           </ul>
