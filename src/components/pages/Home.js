5c7a1665663f691c8a6d2d81296eec63e60f3fbe
Thirstypooch <thirstypooch@protonmail.com>
2021-02-09 19:27:57 -0500

    doing the footer

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import '../../App.css'
-import Cards from '../Cards';
 import HeroSection from '../HeroSection'
+import Cards from '../Cards';
+import Footer from '../Footer'
 
 
 function Home() {
@@ -9,6 +10,7 @@ function Home() {
     <>
       <HeroSection />
       <Cards />
+      <Footer />
     </>
   )
 }
